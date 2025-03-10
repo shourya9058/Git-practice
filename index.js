941c83d04cf245b5da3750f2b99da08e113264fa
Shourya Singh <imshourya07singh@gmail.com>
2025-03-10 21:11:54 +0530

    My first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log("Hello, Shaurya! Your Node.js project is working.");
+
+const figlet = require("figlet");  // Add this line at the top
+
+figlet("Shourya Singh", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
